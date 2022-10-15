3349309d335b406dd0f71785095b42d072f7a571
luhuipeng <973880264@qq.com>
2022-10-15 10:15:06 +0800

    feat: export esm.js & demo(VueUse)

---
+++ src/entry.ts
@@ -0,0 +1,15 @@
+import { App } from "vue";
+import MyButton from "./button";
+import SFCButton from "./SFCButton.vue";
+
+// 导出单独组件
+export { MyButton, SFCButton };
+
+// 编写一个插件，实现一个install 方法
+
+export default {
+  install(app: App): void {
+    app.component(MyButton.name, MyButton);
+    app.component(SFCButton.name, SFCButton);
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
 
 export default defineConfig({
   plugins: [vue(), vueJsx({})],
+  build: {
+    rollupOptions,
+    minify: false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "FilterUI",
+      fileName: "filter-ui",
+      formats: ["esm", "umd", "iife"],
+    },
+  },
 });
