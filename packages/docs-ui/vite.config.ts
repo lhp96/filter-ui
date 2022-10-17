f3ff9ec12714910c9eeab70fe7e206eb79906826
luhuipeng <973880264@qq.com>
2022-10-17 22:25:48 +0800

    refactor: monorepo (1)

--- packages/docs-ui/vite.config.ts
+++ packages/docs-ui/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Unocss from "../config/unocss";
+import Unocss from "../../config/unocss";
 
 export default defineConfig({
   plugins: [vueJsx(), Unocss()],
