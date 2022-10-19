d7f6280f7d1e1b3f3d9fb667fc3aef24c63a9dfa
luhuipeng <973880264@qq.com>
2022-10-19 16:29:35 +0800

    chore: abandon cd

--- docs/.vitepress/config/sidebar.js
+++ docs/.vitepress/config/sidebar.js
@@ -10,21 +10,21 @@ export default {
         { text: "快速上手", link: "/" },
       ],
     },
-    // {
-    //   text: "基础组件",
-    //   items: arr[1].items,
-    // },
-    // {
-    //   text: "导航组件",
-    //   items: arr[2].items,
-    // },
-    // {
-    //   text: "数据展示",
-    //   items: arr[2].items,
-    // },
-    // {
-    //   text: "反馈组件",
-    //   items: arr[2].items,
-    // },
+    {
+      text: "基础组件",
+      items: arr[1].items,
+    },
+    {
+      text: "导航组件",
+      items: arr[2].items,
+    },
+    {
+      text: "数据展示",
+      items: arr[2].items,
+    },
+    {
+      text: "反馈组件",
+      items: arr[2].items,
+    },
   ],
 };
